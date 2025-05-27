27648f1d3fa8e22e745ed5355638497af7667aa2
Yeskendir Salgara <36730580+sorunokoe@users.noreply.github.com>
2025-05-27 10:08:04 +0200

    Add XCFail if no localization (bundle) found on the package level

--- Plugins/QuarkTestsPlugin/TestGenerator/Localization/LocalizationTestGenerator.swift
+++ Plugins/QuarkTestsPlugin/TestGenerator/Localization/LocalizationTestGenerator.swift
@@ -30,7 +30,7 @@ struct LocalizationTestGenerator: TestGenerator {
                 #if SWIFT_PACKAGE
                     let path = Bundle(for: Quark\(viewInfo.name)L10nTests.self).bundleURL.appending(path: "\(target)_\(target).bundle")
                     guard let bundle = Bundle(path: path.relativePath) else {
-                        XC
+                        XCTFail("No bundle found. Make sure there is localized files in your SPM package (/Resources).")
                         return 
                     }
                 #else
